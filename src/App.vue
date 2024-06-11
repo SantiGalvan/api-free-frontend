645e0ba4758d2eb48fbbff6bfc5c5cec9e228522
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-11 10:40:15 +0200

    created app header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { AppHeader }
 }
 </script>
 
 <template>
-
+  <!-- Header -->
+  <AppHeader />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+
+    <header>
+        <nav class="container">
+            <div class="logo">
+                <figure>
+                    <img src="/src/assets/img/logo.jpg" alt="Logo" class="img-fluid rounded-circle">
+                </figure>
+            </div>
+            <div class="links">
+                <ul class="d-flex justify-content-center gap-3">
+                    <li><a href="">Posts</a></li>
+                    <li><a href="">Categories</a></li>
+                    <li><a href="">Tags</a></li>
+                </ul>
+            </div>
+            <div class="actions">
+                <ul class="d-flex justify-content-center gap-3">
+                    <li><a href="">Register</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+</template>
+
+<style lang="scss"></style>
