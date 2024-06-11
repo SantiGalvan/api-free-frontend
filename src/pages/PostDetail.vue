225c4ca781500a20330baec4b10bfe9d7ac80e77
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-11 15:17:41 +0200

    created PostDetail

---
+++ src/pages/PostDetail.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import PostCard from '../components/posts/PostCard.vue';
+const postsUri = 'http://localhost:3000/posts/'
+
+export default {
+    name: 'PostDetail',
+    components: { PostCard },
+    data: () => ({
+        post: {}
+    }),
+    methods: {
+        async getPost() {
+            try {
+                const res = await axios.get(postsUri + this.$route.params.slug);
+                this.post = res.data;
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    },
+    created() {
+        this.getPost();
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <PostCard :post="post" :isDetail="true" />
+    </div>
+</template>
+
+<style lang="scss"></style>
